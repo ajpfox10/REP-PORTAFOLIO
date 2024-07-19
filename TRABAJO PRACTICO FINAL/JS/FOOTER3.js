44801c51d5b8293e33f6b6a9999afb2b1eae4972
ajpfox10 <110938770+ajpfox10@users.noreply.github.com>
2024-07-19 01:34:33 -0300

    Add files via upload

---
+++ TRABAJO PRACTICO FINAL/JS/FOOTER3.js
@@ -0,0 +1,24 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var footer = `
+        <footer>
+            <div class="footer-content">
+                <div class="social-icons">
+                    <a href="https://wa.me/1234567890" target="_blank" class="whatsapp">
+                        <i class="lab la-whatsapp footer-icon"></i>
+                    </a>
+                    <a href="https://www.facebook.com" target="_blank" class="facebook">
+                        <i class="lab la-facebook footer-icon"></i>
+                    </a>
+                    <a href="https://www.instagram.com" target="_blank" class="instagram">
+                        <i class="lab la-instagram footer-icon"></i>
+                    </a>
+                    <a href="https://www.twitter.com" target="_blank" class="twitter">
+                        <i class="lab la-twitter footer-icon"></i>
+                    </a>
+                </div>
+                <button class="cardiology-back-btn" onclick="window.location.href='../index.html'">Volver a la página principal</button>
+            </div>
+        </footer>
+    `;
+    document.body.insertAdjacentHTML('beforeend', footer);
+});
---
+++ TRABAJO PRACTICO FINAL/JS/footer.js
@@ -0,0 +1,23 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var footer = `
+        <footer>
+            <div class="footer-content">
+                <div class="social-icons">
+                    <a href="https://wa.me/1234567890" target="_blank" class="whatsapp">
+                        <i class="lab la-whatsapp"></i>
+                    </a>
+                    <a href="https://www.facebook.com" target="_blank" class="facebook">
+                        <i class="lab la-facebook"></i>
+                    </a>
+                    <a href="https://www.instagram.com" target="_blank" class="instagram">
+                        <i class="lab la-instagram"></i>
+                    </a>
+                    <a href="https://www.twitter.com" target="_blank" class="twitter">
+                        <i class="lab la-twitter"></i>
+                    </a>
+                </div>
+            </div>
+        </footer>
+    `;
+    document.body.insertAdjacentHTML('beforeend', footer);
+});
